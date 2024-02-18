93173d3f791b9955e16b81a5a4263160d500b78c
Jose <jignacio20072@gmail.com>
2024-02-18 01:20:12 -0300

    responsive(falta footer)

---
+++ build/js/main.js
@@ -0,0 +1,40 @@
+// Definir la URL de la API
+const apiUrl = 'https://dolarapi.com/v1/dolares/blue';
+
+fetch(apiUrl)
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+})
+.then(data => {
+    // Asumiendo que quieres usar el precio de venta del dólar para la conversión
+    const precioDolarVenta = parseFloat(data.venta);
+    console.log(precioDolarVenta);
+    //Mostrar precio de dolar en pantalla
+
+    let dolarPantalla = document.getElementsByClassName('dolar_pantalla');
+    dolarPantalla[0].textContent = `$${precioDolarVenta}`;
+    
+    // Calcular el precio en dólares y mostrarlo en la página
+    let cantidadDeMonto = document.querySelectorAll('p.monto').length;
+
+
+for(let i=0; i<cantidadDeMonto; i++){
+    // Obtener el precio en moneda local desde tu página web
+    let precioDolar = document.getElementsByClassName('preciodolar');
+    precioDolar = precioDolar[i].textContent;
+    precioDolar = parseInt(precioDolar);
+
+    let precioEnPesos = precioDolar * precioDolarVenta;
+
+    let elementoMonto = document.querySelectorAll('p.monto')[i];
+
+    elementoMonto.textContent = `A$S ${precioEnPesos}`;
+}
+
+})
+.catch(error => {
+    console.error('Error al obtener los datos de la API:', error);
+});
