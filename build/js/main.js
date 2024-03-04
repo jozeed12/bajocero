e269029941f46a80fcee25204f4993b86f85a7a9
jozeed12 <jignacio20072@gmail.com>
2024-03-04 10:17:53 -0300

    arreglos

--- build/js/main.js
+++ build/js/main.js
@@ -39,3 +39,87 @@ for(let i=0; i<cantidadDeMonto; i++){
     console.error('Error al obtener los datos de la API:', error);
 });
 
+//CARGAR PRODUCTOS
+
+const productosAll = document.querySelector('#productos-all');
+
+document.addEventListener('DOMContentLoaded', () => {
+    mostrarProductos(productos);
+})
+
+function mostrarProductos(productos){
+
+    productos.forEach( producto => {
+        const productoHTML = document.createElement('div');
+
+        productoHTML.innerHTML = `
+            <div class="productos">
+                <div>
+                    <h2 class="h2titulo">${producto.modelo}</h2>
+                </div>
+                <div>     
+                    <img src="${producto.img}" class="imagen_producto">
+                </div>
+                <div class="precio_info">    
+                    <div class="precio">
+                        <p class="oferta"></p>
+                        <p class="monto"></p>
+                        <p class="montoUSD">U$D</p>
+                        <p class="montoUSD preciodolar">${producto.precio}</p>
+                    </div>            
+                    <div class="btn_comprarinfo">
+                        <a class="boton_comprar" 
+                        data-modelo="${producto.modelo}" 
+                        data-img="${producto.img}" 
+                        data-precio="${producto.precio}" 
+                        data-id="${producto.id}">Añadir al carrito</a>                                         
+                    </div>
+                </div>
+            </div>           
+        `;
+
+        productosAll.append(productoHTML);
+    })
+
+    document.querySelectorAll('.boton_comprar').forEach(boton => {
+        boton.addEventListener('click', () => {
+            const modelo = boton.dataset.modelo;
+            const img = boton.dataset.img;
+            const precio = boton.dataset.precio;
+            const id = boton.dataset.id;
+            
+            // Aquí puedes hacer lo que necesites con la información del producto
+            console.log(`Modelo: ${modelo}, Imagen: ${img}, Precio: ${precio}, ID: ${id}`);
+            
+            // Por ejemplo, podrías llamar a una función para agregar el producto al carrito
+            // agregarAlCarrito(modelo, img, precio, id);
+
+            agregarAlCarrito(modelo, precio);
+        });
+    });
+}
+
+function agregarAlCarrito(modelo, precio) {
+    // Crear un nuevo elemento de carrito
+    const itemCarrito = document.createElement('div');
+    itemCarrito.classList.add('item-carrito');
+
+    // Construir el contenido del elemento de carrito
+    itemCarrito.innerHTML = `
+        <p>${modelo}</p>
+        <p>Precio: $${precio}</p>
+    `;
+
+    // Agregar el elemento de carrito al contenedor del carrito
+    const carrito = document.querySelector('#caja-carrito');
+    carrito.appendChild(itemCarrito);
+}
+
+
+
+
+
+
+
+
+
---
+++ build/js/productos.js
@@ -0,0 +1,86 @@
+const productos = [
+	{
+		marca: 'Nike',
+		modelo: 'Nike Dunk Low Off-White Lot 1',
+		precio: 100,
+        img: "build/img/nikedunklot1.png",
+		id: 1
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike Dunk Low Lilac',
+		precio: 95,
+        img: "build/img/nikedunklowlilac.png",
+		id: 2
+	},
+    {
+		marca: 'Adidas',
+		modelo: 'Adidas Forum Low Bad Bunny Pink',
+		precio: 100,
+		img: "build/img/Adidas Forum Low Bad Bunny Pink.png",
+		id: 3
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike SB Dunk Travis Scott',
+		precio: 100,
+		img: "build/img/Nike SB Dunk Travis Scott.png",
+		id: 4
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike Air Jordan 1 Mid Barely Orange',
+		precio: 100,
+		img: "build/img/Nike Air Jordan 1 Mid Barely Orange.png",
+		id: 5
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike Dunk Low Off-White Futura UNC',
+		precio: 100,
+		img: "build/img/Nike Dunk Low Off-White Futura UNC.png",
+		id: 6
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike Air Jordan 1 High Off-White University Blue',
+		precio: 100,
+		img: "build/img/Nike Air Jordan 1 High Off-White University Blue.png",
+		id: 7
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike Air Jordan 1 High Off-White',
+		precio: 100,
+		img: "build/img/Nike Air Jordan 1 High Off-White.png",
+		id: 8
+	},
+    {
+		marca: 'Adidas',
+		modelo: 'Adidas Yezzy Boost 350 V2 Zebra',
+		precio: 100,
+		img: "build/img/Adidas Yezzy Boost 350 V2 Zebra.png",
+		id: 9
+	},
+    {
+		marca: 'Adidas',
+		modelo: 'Adidas Yezzy Boost 350 V2 Oreo',
+		precio: 100,
+		img: "build/img/Adidas Yezzy Boost 350 V2 Oreo.png",
+		id: 10
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike Air Uptempo Black White',
+		precio: 100,
+		img: "build/img/Nike Air Uptempo Black White.png",
+		id: 11
+	},
+    {
+		marca: 'Nike',
+		modelo: 'Nike Air Force 1 Low Double Air',
+		precio: 95,
+		img: "build/img/Nike Air Force 1 Low Double Air.png",
+		id: 12
+	},
+]
