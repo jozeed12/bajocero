ae4fb30b25159ae811aeb6a2dcb04a975ddae1d3
jozeed12 <jignacio20072@gmail.com>
2024-03-04 22:00:11 -0300

    cambios 4/3

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,6 @@
 // Definir la URL de la API
 const apiUrl = 'https://dolarapi.com/v1/dolares/blue';
+let precioDolarVenta = 0;
 
 fetch(apiUrl)
 .then(response => {
@@ -10,7 +11,7 @@ fetch(apiUrl)
 })
 .then(data => {
     // Asumiendo que quieres usar el precio de venta del dólar para la conversión
-    const precioDolarVenta = parseFloat(data.venta);
+    precioDolarVenta = parseFloat(data.venta);
     console.log(precioDolarVenta);
     //Mostrar precio de dolar en pantalla
 
@@ -42,6 +43,11 @@ for(let i=0; i<cantidadDeMonto; i++){
 //CARGAR PRODUCTOS
 
 const productosAll = document.querySelector('#productos-all');
+const carritoTotal = document.querySelector('#caja-carrito');
+const cantidadTotal = document.querySelector('#cantidad');
+const precioTotal = document.querySelector('#precioTotal');
+let totalCantidad = 0;
+let totalPrecio = 0;
 
 document.addEventListener('DOMContentLoaded', () => {
     mostrarProductos(productos);
@@ -49,7 +55,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function mostrarProductos(productos){
 
-    productos.forEach( producto => {
+    productos.forEach(producto => {
         const productoHTML = document.createElement('div');
 
         productoHTML.innerHTML = `
@@ -69,10 +75,9 @@ function mostrarProductos(productos){
                     </div>            
                     <div class="btn_comprarinfo">
                         <a class="boton_comprar" 
-                        data-modelo="${producto.modelo}" 
-                        data-img="${producto.img}" 
-                        data-precio="${producto.precio}" 
-                        data-id="${producto.id}">Añadir al carrito</a>                                         
+                           data-modelo="${producto.modelo}" 
+                           data-precio="${producto.precio}" 
+                           data-id="${producto.id}">Añadir al carrito</a>                                         
                     </div>
                 </div>
             </div>           
@@ -84,15 +89,16 @@ function mostrarProductos(productos){
     document.querySelectorAll('.boton_comprar').forEach(boton => {
         boton.addEventListener('click', () => {
             const modelo = boton.dataset.modelo;
-            const img = boton.dataset.img;
-            const precio = boton.dataset.precio;
+            const precio = parseFloat(boton.dataset.precio);
             const id = boton.dataset.id;
-            
-            // Aquí puedes hacer lo que necesites con la información del producto
-            console.log(`Modelo: ${modelo}, Imagen: ${img}, Precio: ${precio}, ID: ${id}`);
-            
-            // Por ejemplo, podrías llamar a una función para agregar el producto al carrito
-            // agregarAlCarrito(modelo, img, precio, id);
+
+    
+            totalCantidad++;
+            totalPrecio += precio * precioDolarVenta;
+
+            // Actualizar la cantidad y el precio total en el carrito
+            cantidadTotal.textContent = totalCantidad;
+            precioTotal.textContent = `$${totalPrecio.toFixed(2)}`;
 
             agregarAlCarrito(modelo, precio);
         });
@@ -111,9 +117,9 @@ function agregarAlCarrito(modelo, precio) {
     `;
 
     // Agregar el elemento de carrito al contenedor del carrito
-    const carrito = document.querySelector('#caja-carrito');
-    carrito.appendChild(itemCarrito);
+    const carrito = document.querySelector('#info-carrito');
 }
+
 
 
 
